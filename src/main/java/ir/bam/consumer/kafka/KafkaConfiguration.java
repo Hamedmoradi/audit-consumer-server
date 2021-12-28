b8cfc1a7b775a566ba893fb338ea78ed675b9e64
hamed <hamedmoradi.mailsbox.gmail.com>
2021-12-28 15:04:15 +0330

    get Bearer token from kafka json message and add to http header for call audit server

--- src/main/java/ir/bam/consumer/kafka/KafkaConfiguration.java
+++ src/main/java/ir/bam/consumer/kafka/KafkaConfiguration.java
@@ -7,7 +7,6 @@ import java.util.Map;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
--- src/main/java/ir/bam/consumer/kafka/Listener.java
+++ src/main/java/ir/bam/consumer/kafka/Listener.java
@@ -5,6 +5,8 @@ import java.util.concurrent.CountDownLatch;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.http.*;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.TopicPartition;
@@ -12,11 +14,19 @@ import org.springframework.web.client.RestTemplate;
 
 
 @Slf4j
+@ConfigurationProperties(prefix = "spring.kafka.template.default-topic")
 public class Listener {
 
+    @Value("${audit.server.request.url}")
+    private String requstUrl;
+
+    @Value("${audit.server.response.url}")
+    private String responseUrl;
+
+    @Value("${audit.server.methodCall.url}")
+    private String methodCallUrl;
+
     private final RestTemplate restTemplate = new RestTemplate();
-//    @Value("${spring.kafka.template.default-topic}")
-//    private String topic;
 
     public CountDownLatch countDownLatch0 = new CountDownLatch(3);
     public CountDownLatch countDownLatch1 = new CountDownLatch(3);
@@ -26,16 +36,19 @@ public class Listener {
     public void listenPartition0(ConsumerRecord<?, ?> record) {
 //        log.info("Listener Id0, Thread ID: " + Thread.currentThread().getId());
 //        log.info("Received: " + record);
-        saveRequest(separateMessage((String) record.value()));
+        String message = separateMessage((String) record.value());
+        String token = getToken((String) record.value());
+        saveRequest(message, token);
         countDownLatch0.countDown();
-//        System.out.println(topic);
     }
 
     @KafkaListener(id = "id1", topicPartitions = {@TopicPartition(topic = "bmi_audit", partitions = {"1"})})
     public void listenPartition1(ConsumerRecord<?, ?> record) {
 //        log.info("Listener Id1, Thread ID: " + Thread.currentThread().getId());
 //        log.info("Received: " + record);
-        saveResponse(separateMessage((String) record.value()));
+        String message = separateMessage((String) record.value());
+        String token = getToken((String) record.value());
+        saveResponse(message, token);
         countDownLatch1.countDown();
     }
 
@@ -43,38 +56,42 @@ public class Listener {
     public void listenPartition2(ConsumerRecord<?, ?> record) {
 //        log.info("Listener Id2, Thread ID: " + Thread.currentThread().getId());
 //        log.info("Received: " + record);
-        saveMethodCall(separateMessage((String) record.value()));
+        String message = separateMessage((String) record.value());
+        String token = getToken((String) record.value());
+        saveMethodCall(message, token);
         countDownLatch2.countDown();
     }
 
-    private void saveRequest(String message) {
-        String url = "http://localhost:9081/audit/audit/request";
-        auditApiCall(message, url);
-
+    private void saveRequest(String message, String token) {
+        auditApiCall(message, token, requstUrl);
     }
 
-    private void saveResponse(String response) {
-        String url = "http://localhost:9081/audit/audit/response";
-        auditApiCall(response, url);
+    private void saveResponse(String response, String token) {
+        auditApiCall(response, token, responseUrl);
     }
 
-    private void saveMethodCall(String response) {
-        String url = "http://localhost:9081/audit/audit/methodCall";
-        auditApiCall(response, url);
+    private void saveMethodCall(String response, String token) {
+        auditApiCall(response, token, methodCallUrl);
     }
 
-    public String separateMessage(String json) {
+    private void auditApiCall(String message, String token, String url) {
+        String body = message;
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(token);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> request = new HttpEntity<String>(body, headers);
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, request, String.class);
+    }
+
+    private String separateMessage(String json) {
         JSONObject jObject = new JSONObject(json);
         JSONObject messagePart = jObject.getJSONObject("message");
         return String.valueOf(messagePart);
     }
 
-    private void auditApiCall(String message, String url) {
-        String body = message;
-        HttpHeaders headers = new HttpHeaders();
-        headers.setBearerAuth("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJncmFudCI6IlBBU1NXT1JEIiwiaXNzIjoiaHR0cDovL2FwaS5ibWkuaXIvc2VjdXJpdHkiLCJhdWQiOiJrZXkiLCJleHAiOjE2Mzc2NzIzNTk5NjgsIm5iZiI6MTYzNzU4NTk1OTk2OCwicm9sZSI6ImludGVybmV0IGJhbmstY3VzdG9tZXIiLCJzZXJpYWwiOiJkNDc4NGI5ZC02ZjVkLTNhNGMtYTAwZi04ODlkOTNmN2I5NGUiLCJzc24iOiIzOTIwMTgzNTg0IiwiY2xpZW50X2lkIjoiMTIzIiwic2NvcGVzIjpbImFjY291bnQtc3VwZXIiLCJzc28tbWFuYWdlci1jdXN0b21lciIsInNzby1tYW5hZ2VyLWVucm9sbG1lbnQiLCJjdXN0b21lci1zdXBlciJdfQ==.4_58-CWXtWAEUwDV4XcHU10TWoouZxNVTjOD_k_6g-g");
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<String> request = new HttpEntity<String>(body, headers);
-        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, request, String.class);
+    private String getToken(String json) {
+        JSONObject jObject = new JSONObject(json);
+        String tokenPart = jObject.get("token").toString();
+        return tokenPart;
     }
 }
