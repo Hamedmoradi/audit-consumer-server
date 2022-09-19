7489becc8b9ddcf57e9e5ad64dbd3e705a11a668
Hamed Moradi <Ha.Moradi@sadad.co.ir>
2022-09-19 21:45:42 +0430

    BAMD-28511:bug fix
    Module [audit-consumer-server-For-UAT]:
      * Application.java
      - application-dev.yml
      - application.yml
      * bootstrap.properties
      * docker-compose.yml
      * pom.xml

--- src/main/java/ir/bam/consumer/Application.java
+++ src/main/java/ir/bam/consumer/Application.java
@@ -3,9 +3,11 @@ package ir.bam.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
